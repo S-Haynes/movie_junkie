388ba7cfb7bea67f150cea0bf69b6db36f248919
Shaquille Haynes <nothintodoee@yahoo.com>
2018-08-21 06:49:06 -0400

    auth

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://shaynes:movie_junkie123b@ds223812.mlab.com:23812/movie_junkie",
-  secretOrKey: "thisismegaultrasecretcodethatnobodyknowsbutme",
-  API_KEY: "api_key=f8bb5321b2ac567dcd6e70a2e065f9a9"
-};
