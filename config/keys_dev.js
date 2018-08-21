81e2c81549b392bd4aafb4b10abc2e811ba7dd9b
Shaquille Haynes <nothintodoee@yahoo.com>
2018-08-21 01:31:23 -0400

    prepare for deployment

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://shaynes:movie_junkie123b@ds223812.mlab.com:23812/movie_junkie",
+  secretOrKey: "thisismegaultrasecretcodethatnobodyknowsbutme"
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 const keys = require("./config/keys");
 const profileRoutes = require("./routes/api/profile");
 const userRoutes = require("./routes/api/users");
@@ -35,6 +36,15 @@ app.get("/", (req, res) => {
 app.use("/api/profile", profileRoutes);
 app.use("/api/users", userRoutes);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // define port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
